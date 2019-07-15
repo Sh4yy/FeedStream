7743cd91e4236f03df50347b5b594dcb1fb30484
= <shayan@umd.edu>
2019-07-14 20:20:06 -0400

    routes

--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
-from sanic import Blueprint
+from sanic import Blueprint, response
+from sanic.exceptions import abort
 from schema import Schema, Optional
+from controllers import EventProcessor
+
 
 mod = Blueprint('routes', version=1)
 
@@ -26,36 +29,64 @@ unsubscribe_schema = Schema({
 
 
 @mod.post('/publish')
-def publish():
+def publish(request):
     """ publish an event """
-    # { actor, event_id, timestamp, verb }
-    # { actor, event_id, timestamp, verb, target_id }
-    pass
+
+    if not publish_schema.is_valid(request.json):
+        return abort(400, message='invalid request body')
+
+    status = EventProcessor.add_event(request.json)
+    return response.json({'ok': True, 'published': status})
 
 
 @mod.post('/retract')
-def retract():
+def retract(request):
     """ retract an event """
-    # { actor, verb, event_id }
-    pass
 
+    if not retract_schema.is_valid(request.json):
+        abort(400, message='invalid request body')
 
-@mod.get('/consume')
-def consume():
-    # { consumer_id, feed_name, before, after, limit }
-    """ consume a feed by user """
-    pass
+    status = EventProcessor.retract_event(request.json)
+    return response.json({'ok': True, 'retracted': status})
 
 
 @mod.post('/subscribe')
-def subscribe():
+def subscribe(request):
     """ subscribe to a publisher """
-    # { consumer_id, producer_id, feed_name }
-    pass
+
+    if not subscribe_schema.is_valid(request.json):
+        abort(400, message='invalid request body')
+
+    status = EventProcessor.subscribe(
+        event_name=request.json['event_name'],
+        consumer_id=request.json['consumer_id'],
+        producer_id=request.json['producer_id']
+    )
+
+    return response.json({'ok': True, 'subscribed': status})
 
 
 @mod.post('unsubscribe')
-def unsubscribe():
+def unsubscribe(request):
     """ unsubscribe from a publisher """
-    # { consumer_id, producer_id, feed_name }
-    pass
+
+    if not unsubscribe_schema.is_valid(request.json):
+        abort(400, message='invalid request body')
+
+    status = EventProcessor.unsubscribe(
+        event_name=request.json['event_name'],
+        consumer_id=request.json['consumer_id'],
+        producer_id=request.json['producer_id']
+    )
+
+    return response.json({'ok': True, 'unsubscribed': status})
+
+
+@mod.get('/consume')
+def consume(request):
+    """ consume a feed by user """
+
+    if not consume_schema.is_valid(request.json):
+        abort(400, message='invalid request body')
+
+    # todo implement consume
