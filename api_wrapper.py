b194d95e1288e241f9c095fb94e9582748116e40
= <shayan@umd.edu>
2019-07-15 20:30:00 -0400

    added a python api wrapper

---
+++ api_wrapper.py
@@ -0,0 +1,146 @@
+import requests
+
+
+class FeedStreamClient:
+
+    def __init__(self, host, port, version: str = 'v1'):
+        """
+        initialize a new FeedStreamClient
+        :param host: client's host
+        :param port: client's port
+        :param version: version of the api
+        """
+        self._host = host
+        self._port = port
+        self._version = version
+
+    def _post_request(self, method, payload: dict):
+        """
+        make a new post request
+        :param method: request method
+        :param payload: json payload
+        :return: response json
+        """
+
+        url = f"http://{self._host}:{self._port}/{self._version}/{method}"
+        return requests.post(url, json=payload).json()
+
+    def _get_request(self, method, args):
+        """
+        make a new get request
+        :param method: request methods
+        :param args: request arguments
+        :return: response json
+        """
+        url = f"http://{self._host}:{self._port}/{self._version}/{method}"
+        return requests.get(url, params=args).json()
+
+    def publish(self, producer_id: str, item_id: str, verb: str,
+                timestamp: int, consumer_id: str = None):
+        """
+        publish a new item
+        :param producer_id: producer's unique id
+        :param item_id: published item id
+        :param verb: published item's verb
+        :param timestamp: published timestamp (int)
+        :param consumer_id: consumer's id (for activity events)
+        :return: True on success
+        """
+
+        payload = {
+            "producer_id": producer_id,
+            "item_id": item_id,
+            "timestamp": timestamp,
+            "verb": verb,
+        }
+
+        if consumer_id is not None:
+            payload['consumer_id'] = consumer_id
+
+        response = self._post_request('publish', payload=payload)
+        return response['published']
+
+    def retract(self, producer_id: str, item_id: str, verb: str,
+                consumer_id: str = None):
+        """
+        retract a published item
+        :param producer_id: producer's unique id
+        :param item_id: published item's id
+        :param verb: published item's verb
+        :param consumer_id: consumer's id (for activity events)
+        :return: True on success
+        """
+
+        payload = {
+            "producer_id": producer_id,
+            "item_id": item_id,
+            "verb": verb
+        }
+
+        if consumer_id is not None:
+            payload['consumer_id'] = consumer_id
+
+        response = self._post_request('retract', payload=payload)
+        return response['retracted']
+
+    def subscribe(self, event_name: str, producer_id: str, consumer_id: str):
+        """
+        subscribe a consumer to a producer
+        :param event_name: feed's name
+        :param producer_id: producer's unique id
+        :param consumer_id: consumer's unique id
+        :return: True on success
+        """
+
+        payload = {
+            "event_name": event_name,
+            "producer_id": producer_id,
+            "consumer_id": consumer_id
+        }
+
+        response = self._post_request('subscribe', payload=payload)
+        return response['subscribed']
+
+    def unsubscribe(self, event_name: str, producer_id: str, consumer_id: str):
+        """
+        unsubscribe a consumer from a producer
+        :param event_name: feed's name
+        :param producer_id: producer's unique id
+        :param consumer_id: consumer's unique id
+        :return: True on scucess
+        """
+
+        payload = {
+            "event_name": event_name,
+            "producer_id": producer_id,
+            "consumer_id": consumer_id
+        }
+
+        response = self._post_request('unsubscribe', payload=payload)
+        return response['unsubscribe']
+
+    def consume(self, event_name: str, consumer_id: str, limit: int = 20,
+                after: str = None, before: str = None):
+        """
+        consume a feed for a user
+        :param event_name: feed's name
+        :param consumer_id: consumer's id
+        :param limit: limit on result
+        :param after: result after specific id
+        :param before: result before specific id
+        :return: list of {'item_id', 'verb' }
+        """
+
+        args = {
+            "event_name": event_name,
+            "consumer_id": consumer_id,
+            "limit": limit
+        }
+
+        if after is not None:
+            args['after'] = after
+        if before is not None:
+            args['before'] = before
+
+        response = self._get_request('consume', args=args)
+        return response['data']
