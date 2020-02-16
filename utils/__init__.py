8d19be01e2f3c21a90724bc6f660437b8d35587d
shayan <shayan@umd.edu>
2020-02-15 21:17:14 -0500

    added password to config

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,10 +7,12 @@ config = Orange('config.json', auto_dump=True, load=True)
 
 redis = StrictRedis(
     host=config['redis']['host'],
-    port=config['redis']['port'])
+    port=config['redis']['port'],
+    password=config['redis'].get('password'))
 
 db = PostgresqlDatabase(
     config['database']['name'],
     host=config['database']['host'],
     port=config['database']['port'],
-    user=config['database']['user'])
+    user=config['database']['user'],
+    passowrd=config['database'].get('password'))
